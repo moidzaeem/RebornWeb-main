c5f5875065cce5d7473a8d5fefa649c5dfccb858
Muhammad Abid <mabid.se@gmail.com>
2024-09-25 02:30:10 +0500

    updated

--- src/app/(user-profile)/subscriptions/page.jsx
+++ src/app/(user-profile)/subscriptions/page.jsx
@@ -1,4 +1,11 @@
 "use client";
+import priceTag from "@/assets/images/price-tag.png";
+import reduceFootprint from "@/assets/images/ReduceFootprint.png";
+import bigImpact from "@/assets/images/big-impact.png";
+import Image from "next/image";
+import { MenuItem, Select } from "@mui/material";
+// import Select from "react-select";
+
 // import LeftSideBar from "@/components/LeftSideBar";
 // import { redirect } from "next/navigation";
 // import { parseCookies } from "nookies";
@@ -124,6 +131,39 @@ const Page = () => {
   //   </div>
   // );
 
+  const CarbonReductionProjects = [
+    {
+      icon: priceTag,
+      name: "Start Sustainable",
+      detail: "Take the first step towards a sustainable workplace",
+      price: 8.25,
+      positionType: "remote",
+    },
+    {
+      icon: reduceFootprint,
+      name: "Reduce Your Footprint",
+      detail: "Do your part for a greener future",
+      price: 15.5,
+      positionType: "hybrid",
+    },
+    {
+      icon: bigImpact,
+      name: "Big Impact",
+      detail: "Make the biggest difference for the environment",
+      price: 26.7,
+      positionType: "full-time office",
+    },
+  ];
+
+  const numberOfEmployees = [
+    { label: "1 employee", value: 1 },
+    { label: "2 employees", value: 2 },
+    { label: "3 employees", value: 3 },
+    { label: "4 employees", value: 4 },
+    { label: "5 employees", value: 5 },
+    { label: "6 employees", value: 6 },
+  ];
+
   return (
     <div className="w-full flex flex-col gap-4 lg:gap-5">
       {/* ---| Current Subscription Card Mobile |--- */}
@@ -177,6 +217,83 @@ const Page = () => {
             </button>
           </div>
         </div>
+      </div>
+
+      <div className="bg-[#ffffff]/70 flex flex-col items-center py-7 lg:py-9 px-5 md:px-10 xl:px-20 rounded-[20px]">
+        <h3 className="text-[34px] lg:text-[44px] text-green font-medium">
+          Monthly Subscription
+        </h3>
+        <p className="text-sm lg:text-lg text-black font-medium">
+          Fund certified climate projects with high social and humanitarian
+          benefits.
+        </p>
+        <h4 className="mt-4 lg:mt-8.5 text-lg lg:text-3xl text-[#0A033C] font-semibold">
+          Support Carbon Reduction and Removal Projects
+        </h4>
+        <div className="mt-5 grid grid-cols-3 gap-4">
+          {CarbonReductionProjects.map((project, index) => (
+            <div
+              key={index}
+              className="bg-white col-span-3 md:col-span-1 flex flex-col p-5 rounded-[10px]"
+            >
+              <div className="flex flex-col pb-4 border-b border-[#0A033C15]">
+                <Image
+                  src={project.icon}
+                  alt={project.name}
+                  className="w-7.5 h-auto"
+                />
+                <h6 className="mt-4 text-lg lg:text-xl text-[#0A033C] font-semibold">
+                  {project.name}
+                </h6>
+                <p className="mt-2.5 text-[10px] lg:text-sm text-[#0A033C] font-medium">
+                  {project.detail}
+                </p>
+              </div>
+              <div className="mt-3 lg:mt-4.5 flex lg:flex-col items-center lg:items-start justify-between gap-3">
+                <div className="flex flex-col lg:flex-row lg:items-center lg:gap-2.5">
+                  <h5 className="text-2xl lg:text-3xl text-[#0A033C] font-semibold">
+                    <span className="text-base lg:text-xl">£</span>
+                    {project.price}
+                  </h5>
+                  <p className="text-[10px] lg:text-xs text-[#5D5A6F] font-semibold">
+                    Per employee per month
+                  </p>
+                </div>
+                <Select
+                  fullWidth
+                  defaultValue={numberOfEmployees[0].value}
+                  sx={{
+                    // "& .MuiOutlinedInput-notchedOutline": {
+                    //   borderColor: "transparent", // Remove the default border
+                    // },
+                    "&:hover .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "#14A800", // Prevent border color change on hover
+                    },
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                      border: "0px", // borderColor: "#14A800", // Prevent border color change on focus
+                      // boxShadow: "none", // Remove any focus shadow
+                    },
+                  }}
+                  className="border border-green text-green text-base font-medium hover:bg-green hover:text-white font-montserrat"
+                >
+                  {numberOfEmployees.map((i) => (
+                    <MenuItem key={i.value} value={i.value}>
+                      {i.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </div>
+              <p className="mt-2 lg:mt-5 text-[10px] lg:text-xs text-[] font-semibold">
+                Offset the average emissions for a {project.positionType}{" "}
+                employee.
+              </p>
+            </div>
+          ))}
+        </div>
+        <p className="mt-4 lg:mt-6 text-xs lg:text-base text-black font-medium">
+          VCC’s are verified via Gold Standard, Verra, & UN and ensure we adhere
+          to the highest governance and compliance standards in the industry.
+        </p>
       </div>
     </div>
   );
